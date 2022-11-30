8de386ae5a3a21f47e9f016af5d00737f5ceaf1e
Carlos G vanLiempt <carlos.gvl.contact@gmail.com>
2022-11-30 22:34:40 +0100

    web 3 update

--- pruebaweb3/main.js
+++ pruebaweb3/main.js
@@ -49,59 +49,68 @@ const logout = () => {
   
   // 6. when the user clicks the login button run the loginWithEth function
   document.querySelector(".login-btn").addEventListener("click", loginWithEth);  
+// show the user's wallet address from the global userWalletAddress variable
+const showUserWalletAddress = () => {
+  const walletAddressEl = document.querySelector(".wallet-address");
+  walletAddressEl.innerHTML = window.userWalletAddress;
+};
+
+// get the user's wallet balance
+const getWalletBalance = async () => {
+  // check if there is global userWalletAddress variable
+  if (!window.userWalletAddress) {
+    return false;
+  }
+
+  // get the user's wallet balance
+  const balance = await window.web3.eth.getBalance(window.userWalletAddress);
+
+  // convert the balance to ether
+  document.querySelector(".wallet-balance").innerHTML = web3.utils.fromWei(
+    balance,
+    "ether"
+  );
+};
+
 
 // function to show the user dashboard
 const showUserDashboard = async () => {
 
-    // if the user is not logged in - userWalletAddress is null
-    if (!window.userWalletAddress) {
-  
-      // change the page title
-      document.title = "Web3 Login";
-  
-      // show the login section
-      document.querySelector(".login-section").style.display = "flex";
-  
-      // hide the user dashboard section
-      document.querySelector(".dashboard-section").style.display = "none";
-  
-      // return from the function
-      return false;
-    }
-  
+  // if the user is not logged in - userWalletAddress is null
+  if (!window.userWalletAddress) {
+
     // change the page title
-    document.title = "Web3 Dashboard 🤝";
+    document.title = "Web3 Login";
+
+    // show the login section
+    document.querySelector(".login-section").style.display = "flex";
+
+    // hide the user dashboard section
+    document.querySelector(".dashboard-section").style.display = "none";
+
+    // return from the function
+    return false;
+  }
+
+  // change the page title
+  document.title = "Web3 Dashboard 🤝";
+
+  // hide the login section
+  document.querySelector(".login-section").style.display = "none";
+
+  // show the dashboard section
+  document.querySelector(".dashboard-section").style.display = "flex";
+
+  // show the user's wallet address
   
-    // hide the login section
-    document.querySelector(".login-section").style.display = "none";
-  
-    // show the dashboard section
-    document.querySelector(".dashboard-section").style.display = "flex";
-  
-    // show the user's wallet address
-    /// show the user's wallet address from the global userWalletAddress variable
-    const showUserWalletAddress = () => {
-        const walletAddressEl = document.querySelector(".wallet-address");
-        walletAddressEl.innerHTML = window.userWalletAddress;
-    };
-  
-    // get the user's wallet balance
-    // get the user's wallet balance
-    const getWalletBalance = async () => {
-        // check if there is global userWalletAddress variable
-        if (!window.userWalletAddress) {
-        return false;
-        }
-    
-        // get the user's wallet balance
-        const balance = await window.web3.eth.getBalance(window.userWalletAddress);
-    
-        // convert the balance to ether
-        document.querySelector(".wallet-balance").innerHTML = web3.utils.fromWei(
-        balance,
-        "ether"
-        );
-    };
-  
+   showUserWalletAddress();
+
+  // get the user's wallet balance
+   getWalletBalance();
+
+   
 };
+
+
+
   
