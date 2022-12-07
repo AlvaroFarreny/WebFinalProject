e06ac687a2d3fb261c08e3a9125ca8ba7d05d3da
Alvaro Farreny <farrenyalvaro@gmail.com>
2022-12-07 22:43:31 +0100

    sticky scroll

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 // 1. Web3 login function
 const loginWithEth = async () => {
-    // 1.1 check if there is global window.web3 instance
-    if (window.web3) {
-      try {
-        // 2. get the user's ethereum account - prompts metamask to login
-        const selectedAccount = await window.ethereum
-          .request({
-            method: "eth_requestAccounts",
-          })
-          .then((accounts) => accounts[0])
-          .catch(() => {
-            // 2.1 if the user cancels the login prompt
-            throw Error("Please select an account");
-          });
-  
-        // 3. set the global userWalletAddress variable to selected account
-        window.userWalletAddress = selectedAccount;
-  
-        // 4. store the user's wallet address in local storage
-        window.localStorage.setItem("userWalletAddress", selectedAccount);
-  
-        // 5. show the user dashboard
-        showUserDashboard();
-  
-      } catch (error) {
-        alert(error);
-      }
-    } else {
-      alert("wallet not found");
+  // 1.1 check if there is global window.web3 instance
+  if (window.web3) {
+    try {
+      // 2. get the user's ethereum account - prompts metamask to login
+      const selectedAccount = await window.ethereum
+        .request({
+          method: "eth_requestAccounts",
+        })
+        .then((accounts) => accounts[0])
+        .catch(() => {
+          // 2.1 if the user cancels the login prompt
+          throw Error("Please select an account");
+        });
+
+      // 3. set the global userWalletAddress variable to selected account
+      window.userWalletAddress = selectedAccount;
+
+      // 4. store the user's wallet address in local storage
+      window.localStorage.setItem("userWalletAddress", selectedAccount);
+
+      // 5. show the user dashboard
+      showUserDashboard();
+
+    } catch (error) {
+      alert(error);
     }
-  };
+  } else {
+    alert("wallet not found");
+  }
+};
 
 // web3 logout function
 const logout = () => {
-    // set the global userWalletAddress variable to null
-    window.userWalletAddress = null;
-  
-    // remove the user's wallet address from local storage
-    window.localStorage.removeItem("userWalletAddress");
-  
-    // show the user dashboard
-    showUserDashboard();
-  };
-  
-  // when the user clicks the logout button run the logout function
-  document.querySelector(".logout-btn").addEventListener("click", logout);
-  
-  
-  // 6. when the user clicks the login button run the loginWithEth function
-  document.querySelector(".login-btn").addEventListener("click", loginWithEth);  
+  // set the global userWalletAddress variable to null
+  window.userWalletAddress = null;
+
+  // remove the user's wallet address from local storage
+  window.localStorage.removeItem("userWalletAddress");
+
+  // show the user dashboard
+  showUserDashboard();
+};
+
+// when the user clicks the logout button run the logout function
+document.querySelector(".logout-btn").addEventListener("click", logout);
+
+// 6. when the user clicks the login button run the loginWithEth function
+document.querySelector(".login-btn").addEventListener("click", loginWithEth);
+
 // show the user's wallet address from the global userWalletAddress variable
 const showUserWalletAddress = () => {
   const walletAddressEl = document.querySelector(".wallet-address");
@@ -71,7 +71,6 @@ const getWalletBalance = async () => {
     "ether"
   );
 };
-
 
 // function to show the user dashboard
 const showUserDashboard = async () => {
@@ -114,11 +113,11 @@ const showUserDashboard = async () => {
   document.querySelector(".perfil").style.display = "block";
 
   // show the user's wallet address
-  
-   showUserWalletAddress();
+
+  showUserWalletAddress();
 
   // get the user's wallet balance
-   getWalletBalance();
+  getWalletBalance();
 };
 
 function darkmode() {
@@ -127,3 +126,22 @@ function darkmode() {
   element.classList.toggle("texto-blanco");
 };
 
+/*
+// Cambiar barra de navegacion y circulo al bajar
+window.onload = function BarraFija() {
+    if (scroll >= 200) {
+      $(".header").addClass("headerFijo");
+    }
+    else {
+      $(".header").removeClass("headerFijo");
+    }
+};
+*/    
+
+// Cambiar barra de navegacion y circulo al bajar
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.remove("header", window.scrollY > 0);
+  header.classList.toggle("headerFijo", window.scrollY > 0);
+})
